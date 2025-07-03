2cef770c16fa986da1776afc416f2aadf8ad87ea
Tobias Halomoan <agyasta1808@gmail.com>
2025-07-03 21:45:17 +0700

    fix : Enable CORS for back end deployment

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(new LoggerMiddleware().use);
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
